d9f9a26723e4f55713b34d1ea330481a1812d8fc
“{2134523454}” <{21141@student.macleans.school.nz}>
2025-05-12 13:01:53 +1200

    Added Features

--- Gitappxav.py
+++ Gitappxav.py
@@ -1,1 +1,15 @@
- 
+import tkinter as tk
+from tkinter import *
+
+root = tk.Tk() 
+root.title("Voice Bridge")
+root.geometry("1180x644")
+root.minsize(width=300, height=300)
+root.maxsize(width=1180, height=644) 
+root.config(bg="white") 
+
+# Warning! Represents a warning that needs attention.
+# "Danger!" Represents a dangerous negative action.
+
+
+root.mainloop() 
